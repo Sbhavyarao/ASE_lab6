8cec52903f4c866df5e2ff5f8c3ef09a80cbd2f4
Sbhavyarao <bhavyasrirao@gmail.com>
2019-09-28 11:22:13 -0500

    assignment

---
+++ SourceCode/Note App/app.js
@@ -0,0 +1,96 @@
+const fs =  require('fs');
+const yargs = require('yargs');
+const _ = require('lodash');
+const customers = require('./customer.js');
+
+// ------------ Begin - command configuration -----------------
+
+
+const custIdOptions = {
+    describe: 'ID of a customer',
+    demand : true,
+    alias : 'custID'
+}
+
+const custNameOptions = {
+    describe: 'Name of a customer',
+    demand : true,
+    alias : 'custName'
+}
+
+const custEMailOptions = {
+    describe: 'Email of a customer',
+    demand : true,
+    alias : 'custEmail'
+}
+
+const argv =  yargs
+
+    .command('add','Add a new customer',{
+        customer_id: custIdOptions,
+        customer_name: custNameOptions,
+        customer_email:custEMailOptions
+    })
+    .command('list','List all customers')
+    .command('read','Read a customer',{
+        customer_id: custIdOptions
+    })
+    .command('remove','Remove a customer',{
+        customer_id: custIdOptions
+    })
+    .help()
+    .argv;
+
+
+// ------------ End - command configuration -----------------
+
+
+var command = yargs.argv._[0];
+
+
+if (command === 'add'){
+    var customer = customers.addCustomer(argv.custID,argv.custName,argv.custEmail);
+    if (customer){
+        customers.logCustomer(customer);
+    } else{
+        console.log("Customer already exists");
+    }
+}
+
+
+else if (command === 'list') {
+  var AllCustomers = customers.getAll();
+  console.log(`Printing ${AllCustomers.length} customer(s).`);
+    AllCustomers.forEach((customer)=>{
+        customers.logCustomer(customer);
+  });
+}
+
+else if (command === 'update') {
+    var customer = customers.update(argv.custID,argv.custName,argv.custEmail);
+    if(customer){
+        console.log("Customer Updated successfully");
+    }
+    else{
+        console.log("Customer not found");
+    }
+}
+
+else if (command === 'read') {
+   var customer = customers.getCustomer(argv.custID);
+   if(customer){
+       customers.logCustomer(customer);
+          }
+   else{
+    console.log("Customer not found");
+   }
+}
+
+else if (command === 'remove') {
+    var customerRemoved = customers.remove(argv.custID);
+    var message = customerRemoved ? 'Customer was removed' : 'Customer not found';
+    console.log(message);
+}
+else{
+  console.log('command note recognized'); 
+}
---
+++ SourceCode/Note App/customer.js
@@ -0,0 +1,84 @@
+const fs =  require('fs');
+
+var fetchCustomers = () => {
+  try {
+    var customers = fs.readFileSync('customerData.json')
+    return JSON.parse(customers);
+  } catch(e){
+    return [];
+  }
+};
+
+var saveCustomers = (customers) => {
+  fs.writeFileSync('customerData.json',JSON.stringify(customers));
+};
+
+//  to add a new customer
+
+var addCustomer = (customer_id,customer_name, customer_email) => {
+    var customers = fetchCustomers();
+    var customer = {customer_id,customer_name, customer_email}
+
+    var duplicateCustomers =  customers.filter((customer) => {
+        return customer.customer_id === customer_id;
+    });
+
+    if (duplicateCustomers.length === 0){
+        customers.push(customer);
+        saveCustomers(customers);
+        return customer
+    }
+
+};
+
+var getAll = () => {
+    return fetchCustomers();
+};
+
+var getCustomer = (customer_id) => {
+
+    var customers = fetchCustomers();
+
+    var getCustomers =  customers.filter((customer) => {
+      return customer.customer_id === customer_id;
+    });
+
+    return getCustomers[0];
+
+};
+
+var remove = (customer_id) => {
+
+    var customers = fetchCustomers();
+
+    var filteredCustomers =  customers.filter((customer) => {
+        return customer.customer_id !== customer_id;
+    });
+
+    saveCustomers(filteredCustomers);
+    return customers.length !== filteredCustomers.length
+
+};
+var update = (customer_id, customer_name, customer_email) => {
+    var totalCustomers = fetchCustomers();
+     for (var i = 0; i < totalCustomers.length; i++) {
+       if (totalCustomers[i].customer_id === customer_id) {
+         totalCustomers[i].customer_name = customer_name;
+         totalCustomers[i].customer_email = customer_email;
+       }
+     }
+     saveCustomers(totalCustomers);
+     return totalCustomers;
+};
+
+var logCustomer = (customer) => {
+    console.log('--');
+    console.log(`Customer Id: ${customer.customer_id}`);
+    console.log(`Customer Name: ${customer.customer_name}`);
+    console.log(`Customer Email: ${customer.customer_email}`);
+
+};
+
+module.exports = {
+  addCustomer, getAll, remove, update, getCustomer,logCustomer
+};
